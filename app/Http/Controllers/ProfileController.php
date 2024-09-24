c561543868e9b1ce4865d994ecccaa47197dc9c7
gabrielgws.dev <gabrielgws.dev@gmail.com>
2024-09-23 22:20:36 -0300

    HEY-2: installing debug bar and fixs

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -23,7 +23,8 @@ class ProfileController extends Controller
     /**
      * Update the user's profile information.
      */
-    public function update(ProfileUpdateRequest $request
+    public function update(
+        ProfileUpdateRequest $request
     ): RedirectResponse {
         $request->user()->fill($request->validated());
 
