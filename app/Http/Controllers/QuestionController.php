73541eb30ebde87fa4c92c49f3fd781c64a3e195
Gabriel Willian <gabrielgws.dev@gmail.com>
2026-03-17 20:11:11 -0300

    HEY-8: it should be able to create a new question bigger than 255 characters

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Http\Controllers;
+
+use App\Models\Question;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class QuestionController extends Controller
+{
+    public function store(): RedirectResponse
+    {
+        $attributes = request()->validate([
+            'question' => ['required'],
+        ]);
+
+        Question::query()->create($attributes);
+
+        return to_route('dashboard');
+    }
+}
