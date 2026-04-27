d3e9e91a5c0fe0e935296dacad2a196315a900a3
Gabriel Willian <gabrielgws.dev@gmail.com>
2026-04-27 20:27:29 -0300

    hey-11: unlike button

--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -7,15 +7,14 @@
     <span> {{ $question->question }} </span>
     <div>
         <x-form :action="route('question.like', $question)">
-            <button href="{{ route('question.like', $question) }}"
-                    class="flex items-start text-green-500 space-x-2" type="submit">
+            <button class="flex items-start text-green-500 space-x-2" type="submit">
                 <x-icons.thumbs-up class="w-5 h-5 hover:text-green-300 cursor-pointer" />
                 <span> {{ $question->likes }} </span>
             </button>
         </x-form>
 
-        <x-form :action="route('question.like', $question)">
-            <button href="{{ route('question.like', $question) }}" class="flex items-center space-x-2 text-red-500">
+        <x-form :action="route('question.unlike', $question)">
+            <button class="flex items-center space-x-2 text-red-500" type="submit">
                 <x-icons.thumbs-down class="w-5 h-5  hover:text-red-300 cursor-pointer" />
                 <span> {{ $question->unlikes }} </span>
             </button>
